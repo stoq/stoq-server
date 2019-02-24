8e25e363cc13c6bd0f49a892548ded98e8ae90e0
Fabricio C Zuardi <fabricio@fabricio.org>
2019-02-24 16:29:59 -0300

    Minor fix on the version string to tuple conversion
    The old sed pattern would consider anything that came after the tilde
    character as one string value.
    
    This patch changes it to allow beta numbers after the dot character to
    be separated as another tuple item.
    
    Example: "0.26.1~beta.0" becomes "(0, 26, 1, "beta", 0)"
    
    Change-Id: Ic3d4bd6c3db4de7ba8eb6c899ace5e260fe9718e
    Fixes: https://gitlab.com/stoqtech/private/bdil/issues/189

--- create-release.sh
+++ create-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 version=$1
-tuple_version=`echo $version | sed "s/\./, /g" | sed "s/~\(.*\)/, \"\1\"/g"`
+tuple_version=`echo $version | sed "s/\./, /g" | sed "s/~\([^,]*\)/, \"\1\"/g"`
 
 dch -v $1 "Release $version"
 sed -i s/UNRELEASED/xenial/ debian/changelog
