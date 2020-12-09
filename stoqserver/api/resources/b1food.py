4fd5ce54677f6806d7fd2dc43dc5cf43c3fbe8fb
Danilo Henrique Cordeiro <dancorde@gmail.com>
2020-12-09 15:08:54 -0300

    Created B1Food Sale Item resource
    Created endpoint to list items of sales
    
    Fixes: https://gitlab.com/stoqtech/private/stoq-server/-/issues/133

--- stoqserver/api/resources/b1food.py
+++ stoqserver/api/resources/b1food.py
@@ -24,11 +24,16 @@
 
 import logging
 
+from datetime import datetime
 from flask import abort, make_response, jsonify, request
+from storm.expr import And, Join, Or
 
+from stoqlib.domain.person import Branch, Company, Individual, Person
+from stoqlib.domain.sale import Sale
 from stoqlib.lib.configparser import get_config
+from stoqlib.lib.formatters import raw_document
 
-from stoqserver.api.decorators import b1food_login_required
+from stoqserver.api.decorators import store_provider, b1food_login_required
 from stoqserver.lib.baseresource import BaseResource
 
 log = logging.getLogger(__name__)
@@ -46,6 +51,40 @@ def generate_b1food_token(size=128):
 
 
 b1food_token = generate_b1food_token()
+
+
+def _check_required_params(data, required_params):
+    for arg in required_params:
+        if arg not in data:
+            message = 'Missing parameter \'%s\'' % arg
+            log.error(message)
+            abort(400, message)
+
+
+def _get_acronyms(request_branches):
+    acronyms = []
+    if request_branches:
+        request_branches = request_branches.replace('[', '').replace(']', '')
+        request_branches = request_branches.split(',')
+        for acronym in request_branches:
+            acronyms.append('%04d' % int(acronym) + ' -')
+    return acronyms
+
+
+def _get_documents(request_documents):
+    documents = []
+    if request_documents:
+        request_documents = request_documents.replace('[', '').replace(']', '')
+        documents = request_documents.split(',')
+    return documents
+
+
+def _get_invoice_ids(request_invoice_ids):
+    invoice_ids = []
+    if request_invoice_ids:
+        request_invoice_ids = request_invoice_ids.replace('[', '').replace(']', '')
+        invoice_ids = request_invoice_ids.split(',')
+    return invoice_ids
 
 
 class B1foodLoginResource(BaseResource):
@@ -75,3 +114,117 @@ class IncomeCenterResource(BaseResource):
 
     def get(self):
         return []
+
+
+class B1FoodSaleItemResource(BaseResource):
+    method_decorators = [b1food_login_required, store_provider]
+    routes = ['/b1food/itemvenda']
+
+    def get(self, store):
+        data = request.args
+
+        required_params = ['dtinicio', 'dtfim']
+        _check_required_params(data, required_params)
+
+        initial_date = datetime.strptime(data['dtinicio'], '%Y-%m-%d')
+        end_date = datetime.strptime(data['dtfim'], '%Y-%m-%d')
+
+        request_branches = data.get('lojas')
+        request_documents = data.get('consumidores')
+        request_invoice_ids = data.get('operacaocupom')
+
+        acronyms = _get_acronyms(request_branches)
+        documents = _get_documents(request_documents)
+        invoice_ids = _get_invoice_ids(request_invoice_ids)
+
+        if bool(data.get('usarDtMov')) is True:
+            query = And(Sale.open_date >= initial_date, Sale.open_date <= end_date)
+        else:
+            query = And(Sale.confirm_date >= initial_date, Sale.confirm_date <= end_date)
+
+        tables = [Sale]
+
+        if len(acronyms) > 0 or len(documents) > 0:
+            tables.append(Join(Branch, Sale.branch_id == Branch.id))
+
+        if len(acronyms) > 0:
+            query = And(query, Branch.acronym.is_in(acronyms))
+
+        if len(documents) > 0:
+            tables = tables + [Join(Person, Person.id == Branch.person_id),
+                               Join(Individual, Individual.person_id == Person.id),
+                               Join(Company, Company.person_id == Person.id)]
+            query = And(query, Or(Individual.cpf.is_in(documents), Company.cnpj.is_in(documents)))
+
+        if len(invoice_ids) > 0:
+            query = And(query, Sale.invoice_id.is_in(invoice_ids))
+
+        # These args are sent but we do not have them on the domain
+        # 'redes', 'cancelados'
+
+        sales = store.using(*tables).find(Sale, query)
+
+        response = []
+
+        for sale in sales:
+            for item in sale.get_items():
+                discount = item.item_discount
+                sellable = item.sellable
+                station = sale.station
+                salesperson = sale.salesperson
+                document = raw_document(sale.get_client_document())
+
+                if len(document) == 11:
+                    document_type = 'CPF'
+                elif len(document) == 14:
+                    document_type = 'CNPJ'
+                else:
+                    document_type = ''
+
+                res_item = {
+                    'idItemVenda': item.id,
+                    'valorUnitario': float(item.base_price),
+                    'valorBruto': float(item.base_price * item.quantity),
+                    'valorUnitarioLiquido': float(item.price - discount),
+                    'valorLiquido': float((item.price - discount) * item.quantity),
+                    'idOrigem': None,
+                    'codOrigem': None,
+                    'desconto': float(discount),
+                    'acrescimo': 0,
+                    'maquinaId': station.id,
+                    'nomeMaquina': station.name,
+                    'maquinaCod': station.code,
+                    'quantidade': float(item.quantity),
+                    'redeId': sale.branch.person.company.id,
+                    'lojaId': sale.branch.id,
+                    'idMaterial': sellable.id,
+                    'codMaterial': sellable.code,
+                    'descricao': sellable.description,
+                    'grupo': {
+                        'idGrupo': sellable.category.id,
+                        'codigo': sellable.category.id,
+                        'descricao': sellable.category.description,
+                        'idGrupoPai': sellable.category.category_id or '',
+                        'dataAlteracao': '',  # FIXME
+                        'ativo': True,
+                    },
+                    'operacaoId': sale.id,
+                    'atendenteId': salesperson.id,
+                    'atendenteCod': salesperson.person.login_user.username,
+                    'atendenteNome': salesperson.person.name,
+                    'isTaxa': False,
+                    'isRepique': False,
+                    'isGorjeta': False,
+                    'isEntrega': False,  # FIXME maybe should be true if external order
+                    'consumidores': [{
+                        'documento': document,
+                        'tipo': document_type
+                    }],
+                    'cancelado': False,
+                    'dtLancamento': sale.confirm_date.strftime('%Y-%m-%d'),
+                    'horaLancamento': sale.confirm_date.strftime('%H:%M')
+                }
+
+                response.append(res_item)
+
+        return response
--- tests/api/resources/test_b1food.py
+++ tests/api/resources/test_b1food.py
@@ -2,8 +2,30 @@ import json
 import pytest
 
 from unittest import mock
+from datetime import datetime
+
+from stoqlib.domain.station import BranchStation
+from stoqlib.lib.formatters import raw_document
 
 from stoqserver.api.resources.b1food import generate_b1food_token
+
+
+@pytest.fixture
+def sale(example_creator, current_user):
+    test_sale = example_creator.create_sale()
+    test_sale.open_date = datetime.strptime('2020-01-02', '%Y-%m-%d')
+    test_sale.confirm_date = datetime.strptime('2020-01-02', '%Y-%m-%d')
+    sale_item = example_creator.create_sale_item(test_sale)
+    sellable_category = example_creator.create_sellable_category(description='Category 1')
+    sale_item.sellable.category = sellable_category
+    client = example_creator.create_client()
+    client.person.individual.cpf = '737.948.760-40'
+    test_sale.client = client
+    person = example_creator.create_person()
+    person.login_user = current_user
+    test_sale.salesperson.person = person
+
+    return test_sale
 
 
 @pytest.mark.parametrize('size', (1, 10, 30, 128))
@@ -74,3 +96,238 @@ def test_get_income_center_with_wrong_authorization(get_config_mock, b1food_clie
     response = b1food_client.get('b1food/centrosrenda', query_string=query_string)
 
     assert response.status_code == 401
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_without_initial_date_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtfim': '2020-01-01'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 400
+    assert res['message'] == "Missing parameter 'dtinicio'"
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_without_end_date_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 400
+    assert res['message'] == "Missing parameter 'dtfim'"
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_no_sales(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_usarDtMov_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'usarDtMov': 1
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_lojas_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'lojas': 1
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_lojas_as_list_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'lojas': [1, 2, 4]
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_consumidores_as_list_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'consumidores': [97050782033, 70639759000102]
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_consumidores_and_lojas_args(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'consumidores': [97050782033, 70639759000102],
+        'lojas': [1, 2, 4]
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+def test_get_sale_item_with_operacaocupom_as_list_arg(get_config_mock, b1food_client):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-01',
+        'operacaocupom': [
+            'a25dd1ad-7dae-11ea-b5ac-b285fb9a2a4e',
+            '21b5a545-7aa1-11ea-b5ac-b285fb9a2a4e'
+        ]
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert res == []
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+@pytest.mark.usefixtures('mock_new_store')
+def test_get_sale_item_successfully(get_config_mock, b1food_client, store, sale):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-03'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    item = sale.get_items()[0]
+    sellable = item.sellable
+    station = store.get(BranchStation, sale.station_id)
+    salesperson = sale.salesperson
+    document = raw_document(sale.get_client_document())
+
+    assert response.status_code == 200
+    assert res == [{
+        'acrescimo': 0,
+        'atendenteCod': salesperson.person.login_user.username,
+        'atendenteId': salesperson.id,
+        'atendenteNome': salesperson.person.name,
+        'cancelado': False,
+        'codMaterial': '',
+        'codOrigem': None,
+        'consumidores': [{'documento': document, 'tipo': 'CPF'}],
+        'desconto': -90.0,
+        'descricao': 'Description',
+        'dtLancamento': '2020-01-02',
+        'grupo': {
+            'ativo': True,
+            'codigo': sellable.category.id,
+            'dataAlteracao': '',
+            'descricao': sellable.category.description,
+            'idGrupo': sellable.category.id,
+            'idGrupoPai': sellable.category.category_id or ''
+        },
+        'horaLancamento': '00:00',
+        'idItemVenda': item.id,
+        'idMaterial': sellable.id,
+        'idOrigem': None,
+        'isEntrega': False,
+        'isGorjeta': False,
+        'isRepique': False,
+        'isTaxa': False,
+        'lojaId': sale.branch.id,
+        'maquinaCod': station.code,
+        'maquinaId': station.id,
+        'nomeMaquina': station.name,
+        'operacaoId': sale.id,
+        'quantidade': 1.0,
+        'redeId': sale.branch.person.company.id,
+        'valorBruto': 10.0,
+        'valorLiquido': 190.0,
+        'valorUnitario': 10.0,
+        'valorUnitarioLiquido': 190.0
+    }]
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+@pytest.mark.usefixtures('mock_new_store')
+def test_get_sale_item_with_cnpj_client_successfully(get_config_mock, b1food_client,
+                                                     store, example_creator, sale):
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    sale.client.person.individual = None
+    company = example_creator.create_company()
+    company.cnpj = '35.600.423/0001-27'
+    sale.client.person.company = company
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-03'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert res[0]['consumidores'] == [{'documento': '35600423000127', 'tipo': 'CNPJ'}]
+
+
+@mock.patch('stoqserver.api.decorators.get_config')
+@pytest.mark.usefixtures('mock_new_store')
+def test_get_sale_item_with_empty_document(get_config_mock, b1food_client, store, sale):
+    sale.client.person.individual.cpf = ''
+    get_config_mock.return_value.get.return_value = "B1FoodClientId"
+    query_string = {
+        'Authorization': 'Bearer B1FoodClientId',
+        'dtinicio': '2020-01-01',
+        'dtfim': '2020-01-03'
+    }
+    response = b1food_client.get('b1food/itemvenda', query_string=query_string)
+    res = json.loads(response.data.decode('utf-8'))
+
+    assert response.status_code == 200
+    assert res[0]['consumidores'] == [{'documento': '', 'tipo': ''}]
