34192cebe1da20d982798adc39ad7f0a090cda22
luisoishi <luisoishi@stoq.com.br>
2020-12-18 14:13:30 -0300

    DLC42 Add a way to bump debian/changelog.

--- create-release.sh
+++ create-release.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-version=$1
-tuple_version=`echo $version | sed "s/\./, /g" | sed "s/~\([^,]*\)/, \"\1\"/g"`
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, expected 'major', 'minor' or 'patch'"
+    exit 0
+fi
 
-dch -v $1 "Release $version"
+version=$1
+new_version=`bump2version --list --dry-run --allow-dirty $version | tail -n 1 | cut -d'=' -f 2`
+
+dch -v $new_version "Release $new_version"
 sed -i s/UNRELEASED/xenial/ debian/changelog
 
-sed -i "s/__version__ = .*/__version__ = ($tuple_version)/" stoqserver/__init__.py
+git add debian/changelog
+git commit --amend --no-edit
+bump2version $1
 
