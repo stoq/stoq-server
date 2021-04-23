e27eaa4ff03baa133022a95e532f344d9d6f91d8
luis <luisoishi@stoq.com.br>
2021-04-23 18:40:10 -0300

    [PRM-ONE-TST2] Improve sale integration webhook.

--- stoqserver/api/resources/webhook.py
+++ stoqserver/api/resources/webhook.py
@@ -1,22 +1,42 @@
 import logging
 
+from flask import make_response, request
+
+from stoqlib.lib.configparser import get_config
 from stoqserver.lib.baseresource import BaseResource
-from stoqserver.api.decorators import login_required
 from stoqserver.signals import WebhookEvent
 
 log = logging.getLogger(__name__)
 
 
+class IntegrationWebhookException(Exception):
+    pass
+
+
 class WebhookEventResource(BaseResource):
-    method_decorators = [login_required]
     routes = ['/v1/webhooks/event']
 
+    def check_token(self, token):
+        config = get_config()
+        if not config.has_section('Integration'):
+            return False
+
+        return token == config.get('Integration', 'access_token')
+
     def post(self):
+        token = request.headers.get('Authorization')
+        if not self.check_token(token):
+            return make_response('Invalid access token', 401)
+
         data = self.get_json()
         log.info('Webhook event: %s', data)
 
         # An event can have multiple replies
-        replies = [i[1] for i in WebhookEvent.send(data) if [1]]
+        try:
+            replies = [i[1] for i in WebhookEvent.send(data)]
+        except IntegrationWebhookException as err:
+            log.exception('Error')
+            make_response(str(err), 500)
 
         # Get first non false response from the event
         reply = replies[0] if replies else None
--- tests/api/resources/test_webhook.py
+++ tests/api/resources/test_webhook.py
@@ -1,11 +1,22 @@
+from unittest import mock
 import json
 import pytest
 
 from stoqserver.signals import WebhookEvent
 
 
+@pytest.fixture(autouse=True)
+def mock_config(monkeypatch):
+    get_config_mock = mock.Mock()
+    get_config_mock.return_value = mock.Mock()
+    get_config_mock.return_value.has_section.return_value = True
+    get_config_mock.return_value.get.return_value = 'mysecretaccesstoken'
+    monkeypatch.setattr('stoqserver.api.resources.webhook.get_config', get_config_mock)
+
+
 @pytest.mark.usefixtures('mock_new_store')
 def test_post_webhook_without_listeners(client):
+    client.auth_token = 'mysecretaccesstoken'
     response = client.post("/v1/webhooks/event")
     res = json.loads(response.data.decode('utf-8'))
     assert response.status_code == 200
@@ -19,6 +30,7 @@ def test_post_webhook(client):
 
     WebhookEvent.connect(callback)
 
+    client.auth_token = 'mysecretaccesstoken'
     response = client.post("/v1/webhooks/event")
     res = json.loads(response.data.decode('utf-8'))
     assert response.status_code == 200
